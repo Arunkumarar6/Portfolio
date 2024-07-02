361c5033212e119935c464f846df4e3f6202e1ea
Arun Kumar K <arunkumark.ar6@gmail.com>
2024-07-02 23:45:57 +0530

    new

--- src/Components/Section_one/Sectionone.jsx
+++ src/Components/Section_one/Sectionone.jsx
@@ -10,7 +10,7 @@ const Sectionone = () => {
             <img src={profile} />
         </div>
         <div className='right'>
-            <h2>Hi, I Arun</h2>
+            <h2>Hi, I'm Arun</h2>
             <h4>Full Stack Web Developer</h4>
             <p>
                 I specialize in creating responsive and user-friendly web applications using modern technologies like React.js, Node.js, and Express.js. With a passion for problem-solving and attention to detail, I strive to deliver high-quality solutions to meet client needs.
